19354ad90e64d7738ecbb879ad61c8a05eef87d4
Damian Spinek <damian00737@gmail.com>
2015-11-03 18:51:44 +0100

    Add method to decompress ZIP files

--- src/library1/ZIP.java
+++ src/library1/ZIP.java
@@ -5,35 +5,65 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZIP {
 	public static void compressFile(String sourcePath, String destinationPath) {
 		zipSingleFile(new File(sourcePath), destinationPath);
 	}
-	
+
 	private static void zipSingleFile(File file, String zipFileName) {
-        try {
-            FileOutputStream fos = new FileOutputStream(zipFileName);
-            ZipOutputStream zos = new ZipOutputStream(fos);
-            ZipEntry ze = new ZipEntry(file.getName());
-            zos.putNextEntry(ze);
-            FileInputStream fis = new FileInputStream(file);
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = fis.read(buffer)) > 0) {
-                zos.write(buffer, 0, len);
-            }
-             
-            zos.closeEntry();
-            zos.close();
-            fis.close();
-            fos.close();
-            System.out.println(file.getCanonicalPath()+" is zipped to "+zipFileName);
-             
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
- 
-    }
+		try {
+			FileOutputStream fos = new FileOutputStream(zipFileName);
+			ZipOutputStream zos = new ZipOutputStream(fos);
+			ZipEntry ze = new ZipEntry(file.getName());
+			zos.putNextEntry(ze);
+			FileInputStream fis = new FileInputStream(file);
+			byte[] buffer = new byte[1024];
+			int len;
+			while ((len = fis.read(buffer)) > 0) {
+				zos.write(buffer, 0, len);
+			}
+
+			zos.closeEntry();
+			zos.close();
+			fis.close();
+			fos.close();
+			System.out.println(file.getCanonicalPath() + " is zipped to " + zipFileName);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void decompressFile(String zipFilePath, String filePath) throws IOException {
+		String destinationname = filePath;
+		byte[] buf = new byte[1024];
+		ZipInputStream zipinputstream = null;
+		ZipEntry zipentry;
+		zipinputstream = new ZipInputStream(new FileInputStream(zipFilePath));
+		zipentry = zipinputstream.getNextEntry();
+		while (zipentry != null) {
+			String entryName = zipentry.getName();
+			FileOutputStream fileoutputstream;
+			File newFile = new File(entryName);
+			String directory = newFile.getParent();
+
+			if (directory == null) {
+				if (newFile.isDirectory())
+					break;
+			}
+			fileoutputstream = new FileOutputStream(destinationname);
+			int n;
+			while ((n = zipinputstream.read(buf, 0, 1024)) > -1) {
+				fileoutputstream.write(buf, 0, n);
+			}
+			fileoutputstream.close();
+			zipinputstream.closeEntry();
+			zipentry = zipinputstream.getNextEntry();
+		}
+		zipinputstream.close();
+	}
 }

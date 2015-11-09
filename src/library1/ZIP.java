0f8ed48584789f8f92a3dd4af8da57cc118d14c1
Damian Spinek <damian00737@gmail.com>
2015-11-09 19:58:57 +0100

    Modify method to compress file

--- src/library1/ZIP.java
+++ src/library1/ZIP.java
@@ -2,6 +2,7 @@ package library1;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.ZipEntry;
@@ -9,33 +10,27 @@ import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZIP {
-	public static void compressFile(String sourcePath, String destinationPath) {
+	public static void compressFile(String sourcePath, String destinationPath) throws IOException {
 		zipSingleFile(new File(sourcePath), destinationPath);
 	}
 
-	private static void zipSingleFile(File file, String zipFileName) {
-		try {
-			FileOutputStream fos = new FileOutputStream(zipFileName);
-			ZipOutputStream zos = new ZipOutputStream(fos);
-			ZipEntry ze = new ZipEntry(file.getName());
-			zos.putNextEntry(ze);
-			FileInputStream fis = new FileInputStream(file);
-			byte[] buffer = new byte[1024];
-			int len;
-			while ((len = fis.read(buffer)) > 0) {
-				zos.write(buffer, 0, len);
-			}
-
-			zos.closeEntry();
-			zos.close();
-			fis.close();
-			fos.close();
-			System.out.println(file.getCanonicalPath() + " is zipped to " + zipFileName);
-
-		} catch (IOException e) {
-			e.printStackTrace();
+	private static void zipSingleFile(File file, String zipFileName) throws IOException {
+		FileOutputStream fos = new FileOutputStream(zipFileName);
+		ZipOutputStream zos = new ZipOutputStream(fos);
+		ZipEntry ze = new ZipEntry(file.getName());
+		zos.putNextEntry(ze);
+		FileInputStream fis = new FileInputStream(file);
+		byte[] buffer = new byte[1024];
+		int len;
+		while ((len = fis.read(buffer)) > 0) {
+			zos.write(buffer, 0, len);
 		}
 
+		zos.closeEntry();
+		zos.close();
+		fis.close();
+		fos.close();
+		System.out.println(file.getCanonicalPath() + " is zipped to " + zipFileName);
 	}
 
 	public static void decompressFile(String zipFilePath, String filePath) throws IOException {
